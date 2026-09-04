56dbc4b3a474a130375a578640d9101a111108ce
Skylar Abruzese <skylar@abruzese.com>
2026-09-04 18:10:40 -0400

    nvim: remove dep from hls

--- dot_config/nvim/lsp/hls.lua
+++ dot_config/nvim/lsp/hls.lua
@@ -12,20 +12,15 @@
 --- })
 --- ```
 
-local util = require 'lspconfig.util'
-
 ---@type vim.lsp.Config
 return {
   cmd = { 'haskell-language-server-wrapper', '--lsp' },
   filetypes = { 'haskell', 'lhaskell' },
-  root_dir = function(bufnr, on_dir)
-    local fname = vim.api.nvim_buf_get_name(bufnr)
-    on_dir(util.root_pattern('hie.yaml', 'stack.yaml', 'cabal.project', '*.cabal', 'package.yaml')(fname))
-  end,
+  root_markers = { 'hie.yaml', 'stack.yaml', 'cabal.project', '*.cabal', 'package.yaml', '.git' },
   settings = {
     haskell = {
       formattingProvider = 'ormolu',
-      cabalFormattingProvider = 'cabal-fmt',
+      cabalFormattingProvider = 'cabalfmt',
     },
   },
 }
